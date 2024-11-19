8f48186737bf50a641a56455744720ae8e797f28
Liam <mountnRocky@gmail.com>
2024-11-19 08:30:12 -0700

    Moved to proper testing package

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/PirateBot/PirateDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/PirateBot/PirateDrive.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode.PirateBot;
+package org.firstinspires.ftc.teamcode.testing.PirateBot;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
