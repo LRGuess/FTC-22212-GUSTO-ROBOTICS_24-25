b18c7983c822de0d28334396a26357f6bfef5a39
Liam <mountnRocky@gmail.com>
2024-11-19 08:31:35 -0700

    Added Global Robot Constants for use in COMP scripts

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants/Constants.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.Constants;
+
+public class Constants {
+    public static double GateOpenPos = 0.02;
+    public static double GateClosePos = 0.4;
+
+    public static String LeftMotorConfigName = "LeftMotor";
+    public static String RightMotorConfigName = "RightMotor";
+    public static String GateServoConfigName = "gateServo";
+
+    public static int driveEncoderResolution = 538;
+
+    public static int nintyDegreeTurn = 190;
+}
